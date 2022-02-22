c32908f39109a6b225f67bdd4b1aa4aa4ae313d3
sravani <inbox.chandu@gmail.com>
2022-02-22 18:57:33 -0500

    added to local Repo

---
+++ src/Test.java
@@ -0,0 +1,32 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+		System.out.println("I am Sravani");
+	}
+
+}
